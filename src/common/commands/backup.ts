14156d1edc554587956dd0411a7873b9a0097059
SystemFiles <bensykes12@gmail.com>
2021-08-07 12:12:34 -0400

    (ref): applied some code formatting

--- src/common/commands/backup.ts
+++ src/common/commands/backup.ts
@@ -1,95 +1,105 @@
-import ora from "ora"
-import { resolve } from "path/posix"
-import { cwd } from "process"
-import { BackupManager } from "../../lib/backup.js"
-import { DatabaseManager } from "../../lib/database.js"
-import { log } from "../../lib/logger.js"
-import { BackupRecord } from "../types.js"
+import ora from 'ora'
+import { resolve } from 'path/posix'
+import { cwd } from 'process'
+import { BackupManager } from '../../lib/backup.js'
+import { DatabaseManager } from '../../lib/database.js'
+import { log } from '../../lib/logger.js'
+import { BackupRecord } from '../types.js'
 
-export async function makeBackup(name: string, src: string, dest: string, ref?: string): Promise<[ BackupRecord, Error ]> {
-  try {
-    // Validate input
-    if (!name || name.length === 0 || !dest || dest.length === 0) {
-      return [ null, new Error(`Invalid options specified...`) ]
-    }
-    log(`Parameters validated ... deciding which backup method to use ...`)
+export async function makeBackup(
+	name: string,
+	src: string,
+	dest: string,
+	ref?: string
+): Promise<[BackupRecord, Error]> {
+	try {
+		// Validate input
+		if (!name || name.length === 0 || !dest || dest.length === 0) {
+			return [ null, new Error(`Invalid options specified...`) ]
+		}
+		log(`Parameters validated ... deciding which backup method to use ...`)
 
-    // Differential backup
-    if (ref && ref.length > 0) return await differentialBackup(name, src, dest, ref)
+		// Differential backup
+		if (ref && ref.length > 0) return await differentialBackup(name, src, dest, ref)
 
-    // Full backup
-    if (src && src.length > 0) return await fullBackup(name, src, dest)
-  } catch (err) {
-    return [ null, err ]
-  }
+		// Full backup
+		if (src && src.length > 0) return await fullBackup(name, src, dest)
+	} catch (err) {
+		return [ null, err ]
+	}
 }
 
-export async function differentialBackup(name: string, src: string, dest: string, ref: string): Promise<[ BackupRecord, Error ]> {
-  try {
-    log(`Validating reference backup ID ...`)
+export async function differentialBackup(
+	name: string,
+	src: string,
+	dest: string,
+	ref: string
+): Promise<[BackupRecord, Error]> {
+	try {
+		log(`Validating reference backup ID ...`)
 
-    // Check if refID passed or refName
-    let refId = ref
-    if (!ref.match('\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b')) {
-      // Translate ref name to an ID to use
-      log(`Reference backup was not presented as UUID ... attempting to translate to UUID from presumed name ...`)
-      refId = ref
-    }
+		// Check if refID passed or refName
+		let refId = ref
+		if (!ref.match('\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b')) {
+			// Translate ref name to an ID to use
+			log(`Reference backup was not presented as UUID ... attempting to translate to UUID from presumed name ...`)
+			refId = ref
+		}
 
-    // Perform the backup and return
-    const mgr: BackupManager = BackupManager.getInstance()
-    log(`Clearing temporary backup buffers ...`)
-    mgr.clearBuffers()
+		// Perform the backup and return
+		const mgr: BackupManager = BackupManager.getInstance()
+		log(`Clearing temporary backup buffers ...`)
+		mgr.clearBuffers()
 
-    // Loading indicator (UI)
-    const spinner = ora('Creating differential backup ...').start()
-    const res = await mgr.diffBackup(refId, resolve(cwd(), src), name, resolve(cwd(), dest))
-    spinner.stop()
-    return res
-  } catch (err) {
-    return [ null, err ]
-  }
+		// Loading indicator (UI)
+		const spinner = ora('Creating differential backup ...').start()
+		const res = await mgr.diffBackup(refId, resolve(cwd(), src), name, resolve(cwd(), dest))
+		spinner.stop()
+		return res
+	} catch (err) {
+		return [ null, err ]
+	}
 }
 
-export async function fullBackup(name: string, src: string, dest: string): Promise<[ BackupRecord, Error ]> {
-  const mgr: BackupManager = BackupManager.getInstance()
-  log(`Clearing temporary backup buffers ...`)
-  mgr.clearBuffers()
-  
-  // Loading indicator (UI)
-  const spinner = ora('Creating full backup ...').start()
-  const res = await mgr.fullBackup(resolve(cwd(), src), name, resolve(cwd(), dest))
-  spinner.stop()
-  return res
+export async function fullBackup(name: string, src: string, dest: string): Promise<[BackupRecord, Error]> {
+	const mgr: BackupManager = BackupManager.getInstance()
+	log(`Clearing temporary backup buffers ...`)
+	mgr.clearBuffers()
+
+	// Loading indicator (UI)
+	const spinner = ora('Creating full backup ...').start()
+	const res = await mgr.fullBackup(resolve(cwd(), src), name, resolve(cwd(), dest))
+	spinner.stop()
+	return res
 }
 
 export async function listBackups(name?: string): Promise<Error> {
-  const db: DatabaseManager = DatabaseManager.getInstance()
-  const [ records, err ] = await db.findAllRecords()
-  if (err) return err
-  let fRecords = records
+	const db: DatabaseManager = DatabaseManager.getInstance()
+	const [ records, err ] = await db.findAllRecords()
+	if (err) return err
+	let fRecords = records
 
-  if (name && name.length > 0) {
-    // Filter by name
-    fRecords = fRecords.filter((r) => r.name.match(name))
-  }
+	if (name && name.length > 0) {
+		// Filter by name
+		fRecords = fRecords.filter((r) => r.name.match(name))
+	}
 
-  if (fRecords.length === 0) {
-    log(`No records were found ...`)
-    return
-  }
+	if (fRecords.length === 0) {
+		log(`No records were found ...`)
+		return
+	}
 
-  const tableData = fRecords.map((r) => {
-    return {
-      id: r.id,
-      name: r.name,
-      created: r.created,
-      type: r.type,
-      size: `${Math.round(r.bytelength / 1024 / 1024)}MB`,
-    }
-  })
+	const tableData = fRecords.map((r) => {
+		return {
+			id: r.id,
+			name: r.name,
+			created: r.created,
+			type: r.type,
+			size: `${Math.round(r.bytelength / 1024 / 1024)}MB`
+		}
+	})
 
-  // Print the result
-  log(`Found a total of ${fRecords.length} record(s) matching list criteria...`)
-  console.table(tableData)
+	// Print the result
+	log(`Found a total of ${fRecords.length} record(s) matching list criteria...`)
+	console.table(tableData)
 }
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,122 +1,122 @@
 import { JSONFile, Low } from 'lowdb'
 import { BackupRecord, RecordTable, RecordType } from '../common/types.js'
-import { RecordNotFoundException } from '../common/exceptions.js' 
+import { RecordNotFoundException } from '../common/exceptions.js'
 import { AppConfig } from './configuration.js'
 import { DB_KEY } from '../common/constants.js'
 import { resolve } from 'path/posix'
 import { cwd } from 'process'
 
 export class DatabaseManager {
-  private static instance: DatabaseManager
-  private dbClient: Low<RecordType>
+	private static instance: DatabaseManager
+	private dbClient: Low<RecordType>
 
 	private constructor(dbPath: string) {
 		const adapter = new JSONFile<RecordType>(dbPath)
-    this.dbClient = new Low<RecordType>(adapter)
+		this.dbClient = new Low<RecordType>(adapter)
 	}
 
-  public static getInstance(path?: string): DatabaseManager {
-    if (!DatabaseManager.instance) {
-      const [ value, error ] = AppConfig.getInstance().getValue(DB_KEY)
-      if (error) {
-        throw new Error(`Could not create database instance. Reason: ${error.message}`)
-      }
-      DatabaseManager.instance = new DatabaseManager(path ? path : resolve(cwd(), value['path'].toString()))
-    }
+	public static getInstance(path?: string): DatabaseManager {
+		if (!DatabaseManager.instance) {
+			const [ value, error ] = AppConfig.getInstance().getValue(DB_KEY)
+			if (error) {
+				throw new Error(`Could not create database instance. Reason: ${error.message}`)
+			}
+			DatabaseManager.instance = new DatabaseManager(path ? path : resolve(cwd(), value['path'].toString()))
+		}
 
-    return DatabaseManager.instance
-  }
+		return DatabaseManager.instance
+	}
 
-  async init(): Promise<void> {
-    try {
-      // Read data as exists (set db.data content)
-      await this.dbClient.read()
+	async init(): Promise<void> {
+		try {
+			// Read data as exists (set db.data content)
+			await this.dbClient.read()
 
-      // Init data if datafile doesn't exist
-      this.dbClient.data ||= { backups: [], archive: [] }
-      await this.dbClient.write()
-    } catch (err) {
-      throw new Error(`Error occurred when initializing localdb. ${err}`)
-    }
-  }
+			// Init data if datafile doesn't exist
+			this.dbClient.data ||= { backups: [], archive: [] }
+			await this.dbClient.write()
+		} catch (err) {
+			throw new Error(`Error occurred when initializing localdb. ${err}`)
+		}
+	}
 
-  async findAllRecords(): Promise<[ BackupRecord[], Error ]> {
-    try {
-      return [ this.dbClient.data.backups, null ]
-    } catch (err) {
-      return [ null, err ]
-    }
-  }
+	async findAllRecords(): Promise<[BackupRecord[], Error]> {
+		try {
+			return [ this.dbClient.data.backups, null ]
+		} catch (err) {
+			return [ null, err ]
+		}
+	}
 
-  findRecordById(id: string, table: RecordTable = RecordTable.BACKUPS): [BackupRecord, Error] {
-    try {
-      let backup: BackupRecord
-      switch (table) {
-        case RecordTable.BACKUPS:
-          backup = this.dbClient.data.backups.find((backup) => backup.id === id)
-          break;
-        case RecordTable.ARCHIVE:
-          backup = this.dbClient.data.archive.find((backup) => backup.id === id)
-      }
+	findRecordById(id: string, table: RecordTable = RecordTable.BACKUPS): [BackupRecord, Error] {
+		try {
+			let backup: BackupRecord
+			switch (table) {
+				case RecordTable.BACKUPS:
+					backup = this.dbClient.data.backups.find((backup) => backup.id === id)
+					break
+				case RecordTable.ARCHIVE:
+					backup = this.dbClient.data.archive.find((backup) => backup.id === id)
+			}
 
-      if (!backup) {
-        throw new RecordNotFoundException(id, `Record could not be found with Id, ${id}`)
-      }
+			if (!backup) {
+				throw new RecordNotFoundException(id, `Record could not be found with Id, ${id}`)
+			}
 
-      return [backup, null]
-    } catch (err) {
-      return [null, err]
-    }
-  }
+			return [ backup, null ]
+		} catch (err) {
+			return [ null, err ]
+		}
+	}
 
-  async insert(table: RecordTable, data: BackupRecord): Promise<void> {
-    try {
-      switch (table) {
-        case RecordTable.BACKUPS:
-          this.dbClient.data.backups.push({...data})
-          break;
-        case RecordTable.ARCHIVE:
-          this.dbClient.data.archive.push({...data})
-          break;
-        default:
-          break;
-      }
+	async insert(table: RecordTable, data: BackupRecord): Promise<void> {
+		try {
+			switch (table) {
+				case RecordTable.BACKUPS:
+					this.dbClient.data.backups.push({ ...data })
+					break
+				case RecordTable.ARCHIVE:
+					this.dbClient.data.archive.push({ ...data })
+					break
+				default:
+					break
+			}
 
-      await this.dbClient.write()
-    } catch (err) {
-      throw new Error(`Error occurred when inserting new backup record. ${err}`)
-    }
-  }
+			await this.dbClient.write()
+		} catch (err) {
+			throw new Error(`Error occurred when inserting new backup record. ${err}`)
+		}
+	}
 
-  async archive(id: string): Promise<[BackupRecord, Error]> { 
-    try {
-      const [backup, error] = await this.findRecordById(id)
-      if (error) {
-        return [null, error]
-      }
+	async archive(id: string): Promise<[BackupRecord, Error]> {
+		try {
+			const [ backup, error ] = await this.findRecordById(id)
+			if (error) {
+				return [ null, error ]
+			}
 
-      // Perform the migration from backup -> archive
-      await this.insert(RecordTable.ARCHIVE, {...backup})
-      return [await this.delete(backup.id, RecordTable.BACKUPS)[0], null]
-    } catch (err) {
-      return [null, err]
-    }
-  }
+			// Perform the migration from backup -> archive
+			await this.insert(RecordTable.ARCHIVE, { ...backup })
+			return [ await this.delete(backup.id, RecordTable.BACKUPS)[0], null ]
+		} catch (err) {
+			return [ null, err ]
+		}
+	}
 
-  async delete(id: string, table: RecordTable = RecordTable.BACKUPS): Promise<[BackupRecord, Error]> {
-    try {
-      const [backupItem, error] = this.findRecordById(id, table)
-      if (error) {
-        return [null, error]
-      }
+	async delete(id: string, table: RecordTable = RecordTable.BACKUPS): Promise<[BackupRecord, Error]> {
+		try {
+			const [ backupItem, error ] = this.findRecordById(id, table)
+			if (error) {
+				return [ null, error ]
+			}
 
-      // Delete the record
-      this.dbClient.data.backups = this.dbClient.data.backups.filter((backup) => backup.id !== id)
-      await this.dbClient.write()
+			// Delete the record
+			this.dbClient.data.backups = this.dbClient.data.backups.filter((backup) => backup.id !== id)
+			await this.dbClient.write()
 
-      return [backupItem, null]
-    } catch (err) {
-      return [null, err]
-    }
-  }
+			return [ backupItem, null ]
+		} catch (err) {
+			return [ null, err ]
+		}
+	}
 }
